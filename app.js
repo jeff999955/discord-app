ea2ff7a47489970ef821dd0552de9a0349aab36a
Jeff <jeff999955@gmail.com>
2023-03-19 20:30:15 +0800

    Complete workable code

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
 
+const { Configuration, OpenAIApi } = require("openai");
 const { Client, Events, GatewayIntentBits } = require("discord.js");
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages,
+  ],
+});
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -10,10 +17,50 @@ client.on("ready", () => {
 client.on(Events.InteractionCreate, async (interaction) => {
   if (interaction.commandName === "ping") {
     await interaction.reply("Pong!");
-  }
-  if (interaction.isMessageComponent()) {
+  } else if (interaction.isMessageComponent()) {
+    console.info(interaction);
+  } else {
+    console.log("others");
     console.info(interaction);
   }
 });
 
+client.on(Events.MessageCreate, async (message) => {
+  if (message.author.bot) return;
+  if (
+    message.author.id !== process.env.ADMIN_ID &&
+    message.mentions.users.find(
+      (user) => user.id === process.env.CHATGPT_USER_ID
+    ) === undefined
+  ) {
+    return;
+  }
+  const question = message.content.replace("<@1086957904155922482>", "");
+  console.log(`${message.author.username}: ${question}`);
+  if (question === "") return;
+  const answer = await askChatGPT(question);
+  console.log(`${answer.role}: ${answer.content}`);
+  message.channel.send(answer);
+});
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 client.login(process.env.DISCORD_TOKEN);
+
+async function askChatGPT(content) {
+  const request = {
+    role: "user",
+    content: content,
+  };
+
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [request],
+    temperature: 0.4,
+    max_tokens: 1000,
+  });
+  return response.data.choices[0].message;
+}
