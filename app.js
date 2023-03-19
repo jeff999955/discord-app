ba8bc6a401eb7952234cd0f860614e7b5862fc64
Jeff <jeff999955@gmail.com>
2023-03-19 19:08:24 +0800

    Add pinging back for interaction

--- app.js
+++ app.js
@@ -10,3 +10,12 @@ app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 app.listen(PORT, () => {
   console.log("Listening on port", PORT);
 });
+
+app.post("/", function (req, res) {
+  const { type } = req.body;
+  if (type === 1) {
+    res.send({
+      type: 1,
+    });
+  }
+});
