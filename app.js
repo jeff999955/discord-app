a3bb1d80796bd928c22eef7cfd5674b243347a5f
Jeff <jeff999955@gmail.com>
2023-03-19 19:15:40 +0800

    Add ping pong

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import express from "express";
 
+const nacl = require("tweetnacl");
+
 import { VerifyDiscordRequest } from "./utils.js";
 
 const app = express();
@@ -12,10 +14,28 @@ app.listen(PORT, () => {
 });
 
 app.post("/", function (req, res) {
+  const isVerified = verifyMessage(req);
+  if (!isVerified) {
+    res.status(401).send("Bad request signature");
+  }
   const { type } = req.body;
   if (type === 1) {
-    res.send({
+    res.status(200).send({
       type: 1,
     });
+    console.log("Ping received");
+  } else {
+    console.info(req.body);
   }
 });
+
+function verifyMessage(request) {
+  const signature = request.get("X-Signature-Ed25519");
+  const timestamp = request.get("X-Signature-Timestamp");
+
+  return nacl.sign.detached.verify(
+    Buffer.from(timestamp + body),
+    Buffer.from(signature, "hex"),
+    Buffer.from(PUBLIC_KEY, "hex")
+  );
+}
