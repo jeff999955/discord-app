184e64d1bfba552a9e1cfbfb493a2c63fbdda229
Jeff <jeff999955@gmail.com>
2023-03-19 19:48:01 +0800

    Add message component

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 require("dotenv").config();
 
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+  if (interaction.isMessageComponent()) {
+    console.info(interaction);
   }
 });
 
-client.login(process.env.DISCORD_TOKEN)
-
-
+client.login(process.env.DISCORD_TOKEN);
